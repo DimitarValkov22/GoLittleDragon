3b7dcc4a7b3c7800d320fe888caaf470febd77d3
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-10 19:13:23 +0300

    10.8.2021
    1.Importing character

--- GoLittleDragon/Assets/Scripts/CoinCollect.cs
+++ GoLittleDragon/Assets/Scripts/CoinCollect.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinCollect : MonoBehaviour
 {
+    public AudioSource collectSound;
      void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            collectSound.Play();
+            GlobalCoin.coinCount += 1;
             this.gameObject.SetActive(false);
         }
     }
