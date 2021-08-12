5dfaf16388cdb93c21d2055a458cc52dc5cafccd
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-12 18:38:13 +0300

    12.8.2021
    1.UI elements
    2.Respawning menu
    3.Time count import

---
+++ GoLittleDragon/Assets/Scripts/GlobalTime.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GlobalTime : MonoBehaviour
+{
+    public GameObject timeDisplay;
+    public int seconds = 30;
+    public bool deductingTime;
+    public GameObject timeUptext;
+    public GameObject fadeIn;
+    public GameObject thePlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(seconds == -1)
+        {
+            thePlayer.GetComponent<PlayerControls>().enabled = false;
+            seconds = 0;
+            timeUptext.SetActive(true);
+            fadeIn.SetActive(true);
+            StartCoroutine(RespawningLevel());
+        }
+        else
+        {
+            if(deductingTime == false)
+        {
+                deductingTime = true;
+                StartCoroutine(DeductSecond());
+            }
+        }
+    }
+
+    IEnumerator DeductSecond()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        seconds -= 1;
+        timeDisplay.GetComponent<Text>().text = "Time: " + seconds;
+        deductingTime = false;
+    }
+
+    IEnumerator RespawningLevel()
+    {
+        yield return new WaitForSecondsRealtime(3);
+        SceneManager.LoadScene(1);
+    }
+}
