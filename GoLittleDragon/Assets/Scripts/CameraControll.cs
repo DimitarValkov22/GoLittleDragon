1f32d63590b92aa2bbc16a26aa3c9917cce6682a
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-20 13:10:08 +0300

    20.8.2021
    1.Character selection system

--- GoLittleDragon/Assets/Scripts/CameraControll.cs
+++ GoLittleDragon/Assets/Scripts/CameraControll.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CameraControll : MonoBehaviour
 {
     public GameObject thePlayer;
+    public GameObject secondPlayer;
+    public GameObject thirdPlayer;
 
     void Update()
     {
         transform.LookAt(thePlayer.transform);
+        transform.LookAt(secondPlayer.transform);
+        transform.LookAt(thirdPlayer.transform);
     }
 }
