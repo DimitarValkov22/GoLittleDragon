e5fcc2af883968040ae9537e7d194428acbe49a9
Dimitar Valkov <mitaka_1911@abv.bg>
2021-09-06 16:51:44 +0300

    6.9.2021
    1.New levels

---
+++ GoLittleDragon/Assets/Scripts/FollowScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    private GameObject thePlayer;
+    public float followspeed;
+    public float minDistance;
+
+
+    void Update()
+    {
+        thePlayer = GameObject.Find("Player(Clone)");
+    }
+
+    void LateUpdate()
+    {
+        if (thePlayer != null)
+        {
+            this.transform.LookAt(thePlayer.transform.position);
+
+            if (Vector3.Distance(this.transform.position, thePlayer.transform.position) > minDistance)
+            {
+                this.transform.Translate(0f, 0f, followspeed * Time.deltaTime);
+            }
+        }
+    }
+}
